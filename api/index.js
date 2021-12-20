8f6844d6f5db5fa63fdd26d1746ff3a238f1ec96
Serdar <bserdarpiri@gmail.com>
2021-12-21 00:43:16 +0200

    initialized react app and api

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const Hapi = require("@hapi/hapi");
+
+const server = Hapi.server({
+  port: 5000,
+  host: "localhost",
+});
+
+server.route({
+  method: "GET",
+  path: "/",
+  handler: (req, h) => {
+    return "Hello from HapiJS!";
+  },
+});
+
+server.start();
+console.log("Server running on %s", server.info.uri);
