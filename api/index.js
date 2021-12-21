b11306a6255c611c047b8f648c412351938795ac
Serdar <bserdarpiri@gmail.com>
2021-12-22 00:20:02 +0200

    MongoDB connected, DB model and POST endpoit created

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 const Hapi = require("@hapi/hapi");
+const Mongoose = require("mongoose");
 
 const server = Hapi.server({
   port: 5000,
+});
+
+Mongoose.connect("mongodb://localhost:27017");
+
+const MovieModel = Mongoose.model("movie", {
+  title: String,
+  date: Date,
 });
 
 server.route({
@@ -12,5 +20,24 @@ server.route({
   },
 });
 
-server.start();
-console.log("Server running on %s", server.info.uri);
+server.route({
+  method: "POST",
+  path: "/movie",
+  handler: async (req, h) => {
+    try {
+      var movie = new MovieModel(req.payload);
+      var result = await movie.save();
+      return h.response(result);
+    } catch (err) {
+      return h.response(err).code(500);
+    }
+    h(req.payload);
+  },
+});
+
+server.start((error) => {
+  if (error) {
+    throw error;
+  }
+  console.log("Server running on %s", server.info.uri);
+});
